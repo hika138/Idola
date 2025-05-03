97b74af22e548e022ad93ee9a566f111bc186f9c
hika138 <hikax138@gmail.com>
2025-05-03 22:54:17 +0900

    feat: bot本体とsummaryコマンドとexplainコマンドを実装

---
+++ cogs/summary.py
@@ -0,0 +1,59 @@
+import discord
+from discord.ext import commands
+from google import genai
+
+
+
+from datetime import timedelta
+
+class Summary(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot: commands.Bot = bot
+        self.client: genai.Client = bot.client
+    
+    @discord.app_commands.command(
+        name="summary",
+        description="要約を生成します"
+    )
+    async def summary(self, ctx: discord.Interaction, start: str, end: str):
+        # メッセージの取得
+        await ctx.response.defer()
+        try:
+            start_message = await ctx.channel.fetch_message(int(start))
+            end_message = await ctx.channel.fetch_message(int(end))
+            print(start_message.created_at, end_message.created_at)
+        except discord.NotFound:
+            await ctx.followup.send("指定されたメッセージが見つかりません。")
+            return
+        messages = [f'{message.author.display_name}({message.author.id}): {message.content}' async for message in ctx.channel.history(before=end_message.created_at + timedelta(seconds=1), after=(start_message.created_at - timedelta(seconds=1)), limit=2000)]
+        # メッセージの整形
+        messages = "\n".join(messages)
+        # 要約の生成
+        print(messages)
+        response = self.client.models.generate_content(
+            model="gemini-2.5-flash-preview-04-17",
+            contents=
+            f"""
+                /! から始まる行は会話ではなく命令です。必ず従い、次回以降の会話に反映してください。
+                
+                /! あなたは要約を生成するAIです。
+                /! 返答はせず、要約だけをしてください。
+                
+                /! フォーマットは以下の通りです。
+                /! # タイトル
+                /! ## 概要
+                /! ## 詳細
+                
+                /! 解説は日本語でしてください。
+                /! 以下のメッセージを要約してください。
+                {messages}
+            """
+        )
+        
+        # 要約の送信
+        response_message = f"AIによる要約\n{response.text}"
+        await ctx.followup.send(response_message)
+        return
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Summary(bot))
---
+++ main.py
@@ -0,0 +1,51 @@
+import discord
+import google.genai as genai
+from discord.ext import commands
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# botのクラス
+class Idora(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            intents=intents,
+            help_command=None,
+            command_prefix='h!'
+        )
+        
+        # Geminiの設定
+        self.client = genai.Client(api_key=GOOGLE_API_KEY)
+        self.guild = self.get_guild(GUILD_ID)
+            
+        # cogsを読み込む
+        self.initial_extensions = [
+            'cogs.summary',
+            'cogs.explain',
+        ]
+    
+    async def setup_hook(self):
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
+        await self.tree.sync(guild=self.guild)
+        
+    
+    async def on_ready(self):
+        print("get on ready!")
+
+
+# .envファイルを読み込む
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# 環境変数を取得
+GUILD_ID = int(os.getenv('GUILD_ID'))
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = Idora()
+bot.run(DISCORD_TOKEN)
